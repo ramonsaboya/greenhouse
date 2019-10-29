491495b48bc8c69e7abb7d571ff7008bb25a0c94
Ramon Saboya <ramonsaboya@fb.com>
2019-10-29 15:28:03 -0300

    create CI job for all services

--- order-complete-updater/src/main/java/demo/OrderCompleteUpdaterApp.java
+++ order-complete-updater/src/main/java/demo/OrderCompleteUpdaterApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 public class OrderCompleteUpdaterApp {
     public static void main(String[] args) {
         SpringApplication.run(OrderCompleteUpdaterApp.class, args);
