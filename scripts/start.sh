38522e42bbf9968b3f73165eb3ca97465f5160b2
Milena Carneiro <milenasccarneiro@gmail.com>
2019-11-02 20:17:46 -0300

    Alterando o start.sh para incluir o download do setup inicial do ECK

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,3 +9,9 @@ kubectl apply -f kubernetes/payment-distribution.yaml
 kubectl apply -f kubernetes/payment-service.yaml
 kubectl apply -f kubernetes/restaurant-service.yaml
 kubectl apply -f kubernetes/ingress.yaml
+sleep 5
+kubectl apply -f https://download.elastic.co/downloads/eck/1.0.0-beta1/all-in-one.yaml
+sleep 10
+kubectl apply -f kubernetes/elasticsearch.yaml
+sleep 10
+kubectl apply -f kubernetes/kibana.yaml
