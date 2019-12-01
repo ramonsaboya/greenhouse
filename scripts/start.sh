a649d2a31ee882f9dce9c1a89f3deb7787f47c37
Ramon Saboya <ramonsaboyag@gmail.com>
2019-12-01 14:36:45 -0300

    Revert "Stack ELK"

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 minikube addons enable ingress
-kubectl apply -f kubernetes/elasticsearch.yaml
-kubectl apply -f kubernetes/logstash.yaml
-kubectl apply -f kubernetes/kibana.yaml
 kubectl apply -f kubernetes/rabbit.yaml
 kubectl apply -f kubernetes/mongo.yaml
 kubectl apply -f kubernetes/hystrix-dashboard.yaml
@@ -12,5 +9,4 @@ kubectl apply -f kubernetes/order-service.yaml
 kubectl apply -f kubernetes/payment-distribution.yaml
 kubectl apply -f kubernetes/payment-service.yaml
 kubectl apply -f kubernetes/restaurant-service.yaml
-kubectl apply -f kubernetes/ingress-gke.yaml
 kubectl apply -f kubernetes/ingress.yaml
